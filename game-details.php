e0f96f6d54a0e536f1b4c1ab87f43e979c9aecd5
ifotn <rich@infrontofthenet.com>
2021-01-27 13:38:02 -0500

    l2/2 - saving games

--- game-details.php
+++ game-details.php
@@ -11,6 +11,14 @@
         <label for="title">Title: </label>
         <input name="title" id="title" required />
     </fieldset>
+    <fieldset>
+        <label for="releaseYear">Release Year:</label>
+        <input name="releaseYear" id="releaseYear" required type="number" />
+    </fieldset>
+    <fieldset>
+        <label for="rating">Rating:</label>
+        <input name="rating" id="rating" />
+    </fieldset>
     <button>Save</button>
 </form>
 
--- save-game.php
+++ save-game.php
@@ -6,12 +6,31 @@
 </head>
 <body>
 <?php
-// store the title value entered in the form in a variable
+// store the values entered in the form in variables
 $title = $_POST['title'];
+$releaseYear = $_POST['releaseYear'];
+$rating = $_POST['rating'];
 
-// display the game title entered by the user
-echo $title;
+// connect to the db
+$db = new PDO('mysql:host=172.31.22.43;dbname=Rich100', 'Rich100', '');
 
+// set up the SQL INSERT command to add a new game.  : indicates a placeholder or paramter
+$sql = "INSERT INTO games (title, releaseYear, rating) VALUES (:title, :releaseYear, :rating)";
+
+// fill the INSERT parameters with our variables
+// connect the db connection w/the SQL command
+$cmd = $db->prepare($sql);
+$cmd->bindParam(':title', $title, PDO::PARAM_STR, 50);
+$cmd->bindParam(':releaseYear', $releaseYear, PDO::PARAM_INT);
+$cmd->bindParam(':rating', $rating, PDO::PARAM_STR, 10);
+
+// execute the save
+$cmd->execute();
+
+// disconnect
+$db = null;
+
+echo "Game Saved";
 ?>
 </body>
 </html>
