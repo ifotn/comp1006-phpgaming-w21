23822f404c5b3a89e634ab7ea5a14391a0ba26de
ifotn <rich@infrontofthenet.com>
2021-02-03 12:54:34 -0500

    l3/1 - displaying games

---
+++ game-titles.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Game Titles</title>
+</head>
+<body>
+<?php
+// 1. Connect to your AWS db, using the credentials you received via email:  Host – 172.31.22.43 / Database – your-db-name / Username – your-username / Password – your-password
+$db = new PDO('mysql:host=172.31.22.43;dbname=Rich100', 'Rich100', 'Vda787-KJ_');
+
+// 2. Write the SQL Query to read all the records from the games table
+$sql = "select * from games";
+
+// 3. Create a Command variable $cmd then use it to run the SQL Query
+$cmd = $db->prepare($sql);
+$cmd->execute();
+
+// 4. Use the fetchAll() method of the PDO Command variable to store the data into a variable called $games.  See https://www.php.net/manual/en/pdostatement.fetchall.php for details.
+$games = $cmd->fetchAll();
+
+// 5. Use a foreach loop to iterate (cycle) through all the values in the $games variable.  Inside this loop, use an echo command to display the title of each game.  See https://www.php.net/manual/en/control-structures.foreach.php for details.
+foreach ($games as $v){
+    echo $v['title'];
+}
+
+// 6. Disconnect from the database
+$db = null;
+?>
+</body>
+</html>
---
+++ games.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Games</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+</head>
+<body>
+<?php
+// 1. Connect to your AWS db, using the credentials you received via email:  Host – 172.31.22.43 / Database – your-db-name / Username – your-username / Password – your-password
+$db = new PDO('mysql:host=172.31.22.43;dbname=Rich100', 'Rich100', '');
+
+// 2. Write the SQL Query to read all the records from the games table
+$sql = "select * from games";
+
+// 3. Create a Command variable $cmd then use it to run the SQL Query
+$cmd = $db->prepare($sql);
+$cmd->execute();
+
+// 4. Use the fetchAll() method of the PDO Command variable to store the data into a variable called $games.  See https://www.php.net/manual/en/pdostatement.fetchall.php for details.
+$games = $cmd->fetchAll();
+
+// 4a. Create an HTML table to format the games data in a grid
+// use bootstrap table classes to style the table
+echo '<table class="table table-striped table-hover"><thead><th>Title</th><th>Release Year</th><th>Rating</th></thead>';
+
+// 5. Use a foreach loop to iterate (cycle) through all the values in the $games variable.  Inside this loop, use an echo command to display the title of each game.  See https://www.php.net/manual/en/control-structures.foreach.php for details.
+// <tr> creates a new table row; <td> creates a new table cell or column ("table data")
+foreach ($games as $v){
+    echo '<tr><td>' . $v['title'] . '</td>
+        <td>' . $v['releaseYear'] . '</td>
+        <td>' . $v['rating'] . '</td></tr>';
+}
+
+// 5a. Close the table
+echo '</table>';
+
+// 6. Disconnect from the database
+$db = null;
+?>
+</body>
+</html>
+
