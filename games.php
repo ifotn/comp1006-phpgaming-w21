edcdcd75f493a4700f6ea93080f83c136ef7f7d5
ifotn <rich@infrontofthenet.com>
2021-02-24 11:53:11 -0500

    l6/1 - delete start

--- games.php
+++ games.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <title>Games</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <script type="text/javascript" src="js/scripts.js"></script>
 </head>
 <body>
 <?php
 // 1. Connect to your AWS db, using the credentials you received via email:  Host – 172.31.22.43 / Database – your-db-name / Username – your-username / Password – your-password
-$db = new PDO('mysql:host=172.31.22.43;dbname=Rich100', 'Rich100', 'Vda787-KJ_');
+$db = new PDO('mysql:host=172.31.22.43;dbname=Rich100', 'Rich100', '');
 
 // 2. Write the SQL Query to read all the records from the games table
 $sql = "select * from games";
@@ -22,14 +23,16 @@ $games = $cmd->fetchAll();
 
 // 4a. Create an HTML table to format the games data in a grid
 // use bootstrap table classes to style the table
-echo '<table class="table table-striped table-hover"><thead><th>Title</th><th>Release Year</th><th>Rating</th></thead>';
+echo '<table class="table table-striped table-hover"><thead><th>Title</th><th>Release Year</th><th>Rating</th><th>Actions</th></thead>';
 
 // 5. Use a foreach loop to iterate (cycle) through all the values in the $games variable.  Inside this loop, use an echo command to display the title of each game.  See https://www.php.net/manual/en/control-structures.foreach.php for details.
 // <tr> creates a new table row; <td> creates a new table cell or column ("table data")
 foreach ($games as $v){
     echo '<tr><td>' . $v['title'] . '</td>
         <td>' . $v['releaseYear'] . '</td>
-        <td>' . $v['rating'] . '</td></tr>';
+        <td>' . $v['rating'] . '</td>
+        <td><a href="delete-game.php?gameId=' . $v['gameId'] .'" 
+            class="btn btn-danger" onclick="return yaSure();">Delete</a></td></tr>';
 }
 
 // 5a. Close the table
---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// delete confirmation - can be used for any delete
+function yaSure() {
+    // use the js confirm() function: OK = true, Cancel = false
+    return confirm('Are you sure you want to delete this?');
+}
